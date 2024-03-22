edb684524381d1fc85f1d61047cb8042f8b854c3
dbaer2025 <dbaerfla@gmail.com>
2024-03-22 11:02:40 -0400

    https://github.com/CSNSE/sportapp/issues/5

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "sportappb3a0434b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
