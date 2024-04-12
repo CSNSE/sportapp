fd7d86f272271b3fc8fc2e4f58e8239a6425b650
dbaer2025 <dbaerfla@gmail.com>
2024-04-12 11:39:32 -0400

    fix edit team #20

--- src/EditTeam.js
+++ src/EditTeam.js
@@ -1,21 +1,48 @@
-import React from 'react'
+import React, { Component, useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
 import { SportNavBar, UIEditTeam } from "./ui-components";
-import {useParams } from "react-router-dom";
+import {Storage } from "aws-amplify";
+import { generateClient } from "aws-amplify/api";
 import "./App.css";
-
-function EditTeam(){
-    return <Put />;
+import { getTeam } from "./graphql/queries";
+function EditTeam() {
+  return <Put />;
 }
 
-  function Put() {
-    const { cid } = useParams();
-    console.log("found " +(cid));
-    return (
-      <div><header className="App-header">
-        <SportNavBar/>
-        <UIEditTeam idProp= {cid}/>
-        </header></div>
-      );
-    }
+function Put() {
+  const client = generateClient();
+  const { cid } = useParams();
+  const [cr, setMe] = useState({});
+useEffect(() => {
+    const queryData = async () => {
+      const record = cid
+        ? (
+            await client.graphql({
+              query: getTeam.replaceAll("__typename", ""),
+              variables: { id: cid },
+            })
+          )?.data?.getTeam
+        : cr;
+        if (record.image) {
+          record.filename = record.image
+          const url = await Storage.get(record.image);
+          record.image = url;
+          }
+        setMe(record);
+    };
+    queryData();
+  }, [cid, cr]);
 
-export default EditTeam
+
+
+  return (
+    <div>
+      <header className="App-header">
+        <SportNavBar />
+        <UIEditTeam nx={cr} />
+      </header>
+    </div>
+  );
+}
+
+export default EditTeam;
